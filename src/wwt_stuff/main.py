bcdb00cb48cf4030b86a4158646196116c3f0cd8
Eli <88557639+lishaduck@users.noreply.github.com>
2025-03-02 00:13:59 -0600

    feat: implement a tiny easter egg called the homepage

--- src/wwt_stuff/main.py
+++ src/wwt_stuff/main.py
@@ -1,10 +1,15 @@
+from pathlib import Path
+
 import fastapi
+from fastapi.responses import HTMLResponse, Response
 
 from wwt_stuff.phishing_detector.bert_model.bert_run import Prediction
 
 from .fact_checker import fact_check
 from .json_streaming_response import JSONStreamingResponse
 from .phishing_detector.bert_model import bert_run
+
+p = Path(__file__).parent.resolve(strict=True)
 
 app = fastapi.FastAPI()
 
@@ -20,3 +25,16 @@ async def root(website_url: str) -> fastapi.Response:
 @app.get("/phishing")
 async def phishing(subject: str, body: str) -> Prediction:
     return bert_run.predict_email(subject, body)
+
+
+@app.get("/")
+async def home() -> fastapi.Response:
+    return HTMLResponse((p / "index.html").read_text(encoding="utf-8"))
+
+
+@app.get("/ducky.svg", include_in_schema=False)
+async def favicon() -> fastapi.Response:
+    return Response(
+        (p / "ducky.svg").read_text(encoding="utf-8"),
+        media_type="image/svg+xml",
+    )
